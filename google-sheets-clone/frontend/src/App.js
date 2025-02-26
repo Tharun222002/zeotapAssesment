b4a5461db73943315f4832bf60d5c30431dc62c9
Tharun222002 <149359833+Tharun222002@users.noreply.github.com>
2025-02-26 22:21:49 +0530

    Update App.js

--- google-sheets-clone/frontend/src/App.js
+++ google-sheets-clone/frontend/src/App.js
@@ -1,1 +1,14 @@
+import React from "react";
+import Spreadsheet from "./Spreadsheet";
+import "./styles.css";
 
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Google Sheets Clone</h1>
+            <Spreadsheet />
+        </div>
+    );
+}
+
+export default App;
